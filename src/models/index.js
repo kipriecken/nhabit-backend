70eeb6e3d371591b1da2611d2b265806cf14cf4b
Kip <kip.riecken@gmail.com>
2026-04-21 20:47:28 -0400

    update backend to current versions

--- src/models/index.js
+++ src/models/index.js
@@ -1,13 +1,15 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const uri = process.env.DB_URL;
 
-mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  serverSelectionTimeoutMS: 2000,
-}).then(() => console.log('Connected'), (err) => console.log(err));
+mongoose
+  .connect(uri, {
+    serverSelectionTimeoutMS: 2000,
+  })
+  .then(
+    () => console.log("Connected"),
+    (err) => console.log(err),
+  );
 
 module.exports = mongoose;
